89b97a869f2dd2db7f1454f23343ac297921eb16
Corinne Jachelski <cjachelski@gmail.com>
2020-05-04 19:47:29 +0000

    Implemented sys.argv

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -4,7 +4,7 @@ def word_counter(file):
     """Count the number of times a word appears in a file"""
 
     #open file
-    file = open(file)
+    file = open(sys.argv[1])
 
     #create empty dictionary
     word_count = {}
@@ -14,10 +14,20 @@ def word_counter(file):
         #create list of words in line all lowercase
         words = line.rstrip().lower().split() 
 
+        #parse through each word in line
         for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
+            #check if word is all alpha characters
+            if word.isalpha():
+                #append word key to dict if not present, or add 1 to present value
+                word_count[word] = word_count.get(word, 0) + 1
+            else:
+                #strip last character in word if not all alpha
+                word = word[:-1]
+                #then add word key to dict if not present, or add 1 to present value
+                word_count[word] = word_count.get(word, 0) + 1
 
+    #print all key, value pairs
     for word, count in word_count.items():
         print(word, count)
 
-print(word_counter("test.txt"))
+(word_counter(sys.argv[1]))
